6646a3235b52aab1542ca5f6e72b836e0d3bb81d
StefanBrodin <42447271+StefanBrodin@users.noreply.github.com>
2026-09-14 22:37:33 +0200

    Added Swagger
    - Added NuGets to 0_AppWebApi.csproj:
         <PackageReference Include="Swashbuckle.AspNetCore" Version="10.2.3" />
        <PackageReference Include="Microsoft.AspNetCore.Mvc.NewtonsoftJson" Version="10.0.10" />
    
    - Added necessary changes/additions to Program.cs
    
    - Changed launch.json to fire off swagger's URL when launching

--- 0_AppWebApi/Program.cs
+++ 0_AppWebApi/Program.cs
@@ -5,22 +5,41 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<ICarsService, CarsService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
 
-// builder.Services.AddControllers();
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Attraction Vote API",
+#if DEBUG
+        Version = "v1.0 DEBUG",
+#else
+        Version = "v1.0",
+#endif
+        Description = "This is the Swagger/OpenAPI documentation for the \"Attraction Vote\" API by Stefan Brodin. It provides information about the available endpoints, request/response formats, and authentication requirements.",
+    });
+});
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// for the purpose of this example, we will use Swagger also in production
+//if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Attraction Vote API v1.0");
+    });
 }
 
 app.UseHttpsRedirection();
